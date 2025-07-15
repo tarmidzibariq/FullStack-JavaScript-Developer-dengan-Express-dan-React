1009f9a16ccb5f19f832bf85088fa08d035943b6
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-07-15 16:44:59 +0700

    Installasi &  Persiapan Backend (Express)

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,8 +1,23 @@
 // import express
 const express = require('express');
 
+// import cors
+const cors = require('cors');
+
+// import body-parser
+const bodyParser = require('body-parser');
+
 // init app
 const app = express();
+
+// use cors
+app.use(cors());
+
+//  use body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // set port
 const port = 3000;
