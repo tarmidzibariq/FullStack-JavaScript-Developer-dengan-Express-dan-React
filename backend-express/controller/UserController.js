542284e8c790176f60a60c29a597a16e14b0aeaa
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-07-17 00:21:37 +0700

    Membuat Restful API Insert Data User

--- backend-express/controller/UserController.js
+++ backend-express/controller/UserController.js
@@ -4,6 +4,12 @@ const express = require('express');
 // import prisma client
 const prisma = require('../prisma/client');
 
+// import validationResult from express-validator
+const { validationResult } = require('express-validator');
+
+// import bcrypt for password hashing
+const bcrypt = require('bcryptjs');
+
 // function findUser
 const findUser = async (req, res) =>{
     try{
@@ -35,4 +41,48 @@ const findUser = async (req, res) =>{
         });
     }
 }
-module.exports = { findUser };
+
+// function createUser
+const createUser = async (req, res) => {
+
+    // periksa hasil validasi
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        // jika ada error, kembalikan error ke pengguna
+        return res.status(422).json({ 
+            success: false,
+            message: 'Validation error',
+            errors: errors.array() 
+        });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    try {
+        // insert data
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+            }
+        });
+
+        res.status(201).send({
+            success: true,
+            message: 'User created successfully',
+            data: user,
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+
+}
+
+module.exports = { findUser, createUser };
--- backend-express/router/index.js
+++ backend-express/router/index.js
@@ -19,6 +19,9 @@ const userController = require('../controller/UserController');
 // import validate register  
 const { validateRegister, validateLogin } = require('../utils/validators/auth');
 
+// import validate user
+const { validateUser } = require('../utils/validators/user');
+
 // define route  for register
 router.post('/register', validateRegister, registerController.register);
 
@@ -27,6 +30,9 @@ router.post('/login', validateLogin, loginController.login);
 
 // define route for find user
 router.get('/admin/users', verifyToken, userController.findUser);
+
+// define route for create user
+router.post('/admin/users', verifyToken, validateUser, userController.createUser);
 
 // module exports router
 module.exports = router;
